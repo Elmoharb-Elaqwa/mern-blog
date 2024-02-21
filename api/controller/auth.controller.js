2ee2f4b0b3d58d36c5d1c89834aae4ef1eccca2a
AbdulrahmanWork <abdulrahmanwork1999@gmail.com>
2024-02-21 19:19:53 +0200

    finish signin function in backend

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
   try {
@@ -28,6 +29,37 @@ export const getUsers = async (req, res, next) => {
   try {
     const users = await User.find({});
     res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password || email == '' || password == '') {
+      res.status(400).json({ message: 'All fields are required' });
+    } else {
+      const validUser = await User.findOne({ email });
+      if (!validUser) {
+        res.status(400).json({ message: 'Email not registered yet !' });
+      } else {
+        const comparedPassword = bcryptjs.compareSync(
+          password,
+          validUser.password
+        );
+        if (!comparedPassword) {
+          res.status(400).json({ message: 'Wrong Credentials !' });
+        } else {
+          const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+          const { password: pass, ...userInfo } = validUser._doc;
+          res
+            .status(200)
+            .cookie('access_token', token, { httpOnly: true })
+            .json({ userInfo, token });
+        }
+      }
+    }
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getUsers, signup } from '../controller/auth.controller.js';
+import { getUsers, signin, signup } from '../controller/auth.controller.js';
 const router = express.Router();
 
 router.post('/signup', signup);
 router.get('/getUsers', getUsers);
+router.post('/signin', signin);
 
 export default router;
